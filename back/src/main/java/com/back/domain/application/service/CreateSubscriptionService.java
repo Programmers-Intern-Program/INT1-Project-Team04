17e01f0c23fd2d0623926a9ebb3eb7a1b19c1a4a
김효신 <kimhyoshin67@gachon.ac.kr>
2026-04-23 15:36:31 +0900

    Feat : 멀티턴 파싱 기능 (초안)

--- back/src/main/java/com/back/domain/application/service/CreateSubscriptionService.java
+++ back/src/main/java/com/back/domain/application/service/CreateSubscriptionService.java
@@ -44,6 +44,7 @@ public class CreateSubscriptionService implements CreateSubscriptionUseCase {
                 user,
                 domain,
                 command.query(),
+                "create",
                 true,
                 null
         ));
