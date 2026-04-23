4e68632ac00c379b8b761e68e7684140fb9e8432
gurum505 <sayhojeong505@gmail.com>
2026-04-23 10:01:44 +0900

    feat : jacoco 프로그인 추가 jacoco설정이 없어 ci test 실패하는 문제 해결

--- back/build.gradle.kts
+++ back/build.gradle.kts
@@ -2,6 +2,7 @@ import org.gradle.kotlin.dsl.testImplementation
 
 plugins {
     java
+    jacoco
     id("org.springframework.boot") version "4.0.5"
     id("io.spring.dependency-management") version "1.1.7"
 }
@@ -27,33 +28,26 @@ dependencyManagement {
 }
 
 dependencies {
-    dependencies {
-        // Web & Core
-        implementation("org.springframework.boot:spring-boot-starter-webmvc")
-        implementation("org.springframework.boot:spring-boot-starter-validation")
-        implementation("org.springframework.boot:spring-boot-starter-actuator")
-        compileOnly("org.projectlombok:lombok")
-        annotationProcessor("org.projectlombok:lombok")
+    // Web & Core
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 
+    // DB
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("org.postgresql:postgresql")
 
-        // DB
-        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-        runtimeOnly("org.postgresql:postgresql")
-        developmentOnly("org.testcontainers:postgresql")
-        developmentOnly("org.testcontainers:jdbc")
-
-        // 테스트 환경
-        testImplementation("org.springframework.boot:spring-boot-starter-test")
-        testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-        testImplementation("org.springframework.boot:spring-boot-starter-data-jpa")
-        testImplementation("org.testcontainers:junit-jupiter")
-        testImplementation("org.testcontainers:postgresql")
-        testImplementation("org.testcontainers:jdbc")
-        testCompileOnly("org.projectlombok:lombok")
-        testAnnotationProcessor("org.projectlombok:lombok")
-        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    }
-
+    // 테스트 환경
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.testcontainers:jdbc")
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.named<Jar>("jar") {
@@ -62,4 +56,13 @@ tasks.named<Jar>("jar") {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = true
+        html.required = true
+    }
 }
