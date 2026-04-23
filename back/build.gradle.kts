98ed5cb675d2db67eb2926636ee9a111f6820cba
gurum505 <sayhojeong505@gmail.com>
2026-04-23 10:40:53 +0900

    feat : testContainer 설정 추가

--- back/build.gradle.kts
+++ back/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
         testImplementation("org.springframework.boot:spring-boot-starter-test")
         testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
         testImplementation("org.springframework.boot:spring-boot-starter-data-jpa")
+        testImplementation("org.springframework.boot:spring-boot-testcontainers")
         testImplementation("org.testcontainers:junit-jupiter")
         testImplementation("org.testcontainers:postgresql")
         testImplementation("org.testcontainers:jdbc")
---
+++ back/src/test/java/com/back/support/IntegrationTestBase.java
@@ -0,0 +1,29 @@
+package com.back.support;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.testcontainers.utility.TestcontainersConfiguration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.time.Clock;
+
+@IntegrationTest
+public class IntegrationTestBase {
+    /*
+    * 중복 많이 되는 MockBean 선언 위치
+    * */
+    @MockitoBean
+    protected Clock clock;
+
+}
+
