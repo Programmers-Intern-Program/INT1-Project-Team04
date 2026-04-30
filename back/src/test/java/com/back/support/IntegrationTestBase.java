791e0f866f27bcdae72d068b7cb3717132d33d47
gurum505 <sayhojeong505@gmail.com>
2026-04-30 11:53:43 +0900

    fix : vertexAI로 바꾸면서 test도 수정

--- back/src/test/java/com/back/support/IntegrationTestBase.java
+++ back/src/test/java/com/back/support/IntegrationTestBase.java
@@ -1,5 +1,6 @@
 package com.back.support;
 
+import com.back.domain.application.port.out.ParseNaturalLanguagePort;
 import java.time.Clock;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,12 @@ public abstract class IntegrationTestBase {
      */
     @MockitoBean
     protected Clock clock;
+
+    // VertexAiTaskParserAdapter가 @Profile("!test")로 비활성화되어 있으므로
+    // 전체 컨텍스트를 로드하는 통합 테스트에서 ParseNaturalLanguagePort 빈이 없어 실패하는 것을 방지.
+    // ParseTaskControllerTest처럼 자체 mock이 있는 테스트는 @MockitoBean이 그 빈을 대체하므로 영향 없음.
+    @MockitoBean
+    protected ParseNaturalLanguagePort parseNaturalLanguagePort;
 
     @AfterEach
     void cleanupDatabase() {
