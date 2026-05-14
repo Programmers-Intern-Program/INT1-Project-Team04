340b13f91985e8f089e7777981124c6b3deff989
gurum505 <sayhojeong505@gmail.com>
2026-05-14 09:33:25 +0900

    feat : errorlog추가2

--- back/src/main/java/com/back/domain/adapter/in/scheduler/ScheduleTrigger.java
+++ back/src/main/java/com/back/domain/adapter/in/scheduler/ScheduleTrigger.java
@@ -2,12 +2,14 @@ package com.back.domain.adapter.in.scheduler;
 
 import com.back.domain.application.port.in.RunSubscriptionMonitorUseCase;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
  * [Incoming Adapter] 시스템 내부의 비즈니스 로직을 주기적으로 실행시키는 스케줄러 trigger
  */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ScheduleTrigger {
@@ -16,6 +18,10 @@ public class ScheduleTrigger {
 
     @Scheduled(fixedDelayString = "${schedule.monitor.fixed-delay-ms:300000}")
     public void runMonitor() {
-        runSubscriptionMonitorUseCase.runAll();
+        try {
+            runSubscriptionMonitorUseCase.runAll();
+        } catch (Exception e) {
+            log.error("[ScheduleTrigger] 구독 모니터 실행 실패", e);
+        }
     }
 }
